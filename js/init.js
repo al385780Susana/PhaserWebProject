03160782b42be4f9c75b290c88672531b55733b4
adriamiralozano <al397094@uji.es>
2024-05-01 19:33:19 +0200

    Menú Principal provisional

--- js/init.js
+++ js/init.js
@@ -1,14 +1,45 @@
 let initState = {
-    preload: preloadInit,
+    preload: loadAssets,
     create: createInit,
     update: updateInit
 };
 
-function preloadInit() {
-   
+
+
+/*
+  Load the assets
+*/
+
+function loadAssets() {
+    game.load.image('mainMenu', 'assets/mainMenu.png');
+    game.load.image('playButton', 'assets/playButton.png');
 }
 
+function createInit() {
 
-function createInit() {}
+    let posX = game.world.width-800;
+    let posY = game.world.height-600;
+    game.add.image(posX, posY, 'mainMenu');
+
+    //let button1 = game.add.image(700, 500, 'playButton');
+    //button1.scale.setTo(1.5);
+
+    //background.anchor.setTo(0.5, 0.5);
+    //background.scale.setTo(1.0);
+    /*
+    textI = 'Despres de posar el fons\n';
+    styleI = {font:'20px Arial', fill:'#FFFFFF'};
+    instructions = game.add.text(40, 80, textI, styleI);
+    */
+    btnStart = game.add.button(700, 500, 'playButton', clickStart);
+    btnStart.anchor.setTo(0.5, 0.5);
+    btnStart.scale.setTo(1.5);
+}
+
+function clickStart() {
+    btnStart.inputEnabled = false;
+    game.state.start('play');
+
+}
 
 function updateInit(){}
--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ let game = new Phaser.Game(GAME_STAGE_WIDTH, GAME_STAGE_HEIGHT, Phaser.AUTO, 'ga
 window.onload = startGame;
 
 function startGame() {
-   // game.state.add('init', initState);
+    game.state.add('init', initState);
     game.state.add('play', playState);
     game.state.add('gameOver', gameOverState);
     game.state.add('win', winState);
-    game.state.start('play');
-    //game.state.start('init');
+    //game.state.start('play');
+    game.state.start('init');
 }
