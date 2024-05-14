d75de0d015632c6356beaa81c485389fa0132f0c
adriamiralozano <al397094@uji.es>
2024-05-14 23:31:15 +0200

    ajuste privisional pantalla inicio

--- js/init.js
+++ js/init.js
@@ -14,26 +14,35 @@ function loadAssets() {
     game.load.image('mainMenu', 'assets/mainMenu.png');
     game.load.image('playButton', 'assets/playButton.png');
     game.load.image('tutorialButton', 'assets/tutorialButton.png');
+    game.load.image('Nave', 'assets/NaveInicio.png')
+    game.load.image('NaveSombra', 'assets/NaveInicioSombra.png');
+    game.load.image('titulo', 'assets/titulo.png');
 }
 
 function createInit() {
 
     game.add.image(0, 0, 'mainMenu');
 
+    titulo = game.add.image(405, 50, 'titulo');
+    titulo.anchor.setTo(0.5, 0.5);
 
-    /*
-    textI = 'prueba texto\n';
-    styleI = {font:'20px Arial', fill:'#FFFFFF'};
-    instructions = game.add.text(40, 80, textI, styleI);
-    */
+    NaveSombra = game.add.image(550, 320, 'NaveSombra');
+    NaveSombra.anchor.setTo(0.5, 0.5);
 
-    btnStart = game.add.button(200, 500, 'playButton', clickStart);
+    Nave = game.add.image(550, 300, 'Nave');
+    Nave.anchor.setTo(0.5, 0.5);
+
+    startMovingAnimation();
+
+    btnStart = game.add.button(130, 400, 'playButton', clickStart);
     btnStart.anchor.setTo(0.5, 0.5);
-    btnStart.scale.setTo(1.0);
+    btnStart.scale.setTo(0.7);
 
-    btnTuto = game.add.button(600, 500, 'tutorialButton', clickTutorial);
+    btnTuto = game.add.button(150, 500, 'tutorialButton', clickTutorial);
     btnTuto.anchor.setTo(0.5, 0.5);
-    btnTuto.scale.setTo(1.0);
+    btnTuto.scale.setTo(0.7);
+
+
 }
 
 function clickStart() {
@@ -47,3 +56,10 @@ function clickTutorial() {
 }
 
 function updateInit(){}
+
+function startMovingAnimation() {
+    var animNave = game.add.tween(Nave.position);
+    animNave.to({
+        y: Nave.y - 20
+    }, 1000, Phaser.Easing.Cubic.Out, true, 0, -1, true);
+}
