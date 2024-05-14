e531d9c2eeee3dd5af54ebd72c1ac84a70481f74
adriamiralozano <al397094@uji.es>
2024-05-15 01:04:45 +0200

    reajustes pantalla inicio

--- js/init.js
+++ js/init.js
@@ -17,24 +17,34 @@ function loadAssets() {
     game.load.image('Nave', 'assets/NaveInicio.png')
     game.load.image('NaveSombra', 'assets/NaveInicioSombra.png');
     game.load.image('titulo', 'assets/titulo.png');
+    game.load.image('creditos', 'assets/creditos.png');
+    game.load.image('dificultad', 'assets/dificultad.png');
 }
 
 function createInit() {
 
     game.add.image(0, 0, 'mainMenu');
 
-    titulo = game.add.image(405, 50, 'titulo');
+    titulo = game.add.image(405, 70, 'titulo');
     titulo.anchor.setTo(0.5, 0.5);
 
-    NaveSombra = game.add.image(550, 320, 'NaveSombra');
+    NaveSombra = game.add.image(550, 360, 'NaveSombra');
     NaveSombra.anchor.setTo(0.5, 0.5);
 
-    Nave = game.add.image(550, 300, 'Nave');
+    Nave = game.add.image(550, 350, 'Nave');
     Nave.anchor.setTo(0.5, 0.5);
-
+    
     startMovingAnimation();
 
-    btnStart = game.add.button(130, 400, 'playButton', clickStart);
+    creditos = game.add.button(160, 420, 'creditos', clickStart);
+    creditos.anchor.setTo(0.5, 0.5);
+    creditos.scale.setTo(1);
+
+    dificultad = game.add.button(175, 340, 'dificultad', clickStart);
+    dificultad.anchor.setTo(0.5, 0.5);
+    dificultad.scale.setTo(1.0);
+
+    btnStart = game.add.button(130, 260, 'playButton', clickStart);
     btnStart.anchor.setTo(0.5, 0.5);
     btnStart.scale.setTo(0.7);
 
